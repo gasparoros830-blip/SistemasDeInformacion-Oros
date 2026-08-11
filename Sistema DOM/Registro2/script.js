cfdf41492317f0f1c6d24b5394a779a5c4d2b515
gasparoros830-blip <gasparoros830@gmail.com>
2026-08-11 14:24:24 -0300

    Add files via upload

---
+++ Sistema DOM/Registro2/script.js
@@ -0,0 +1,20 @@
+class Alumno {
+    constructor(nombre, legajo) {
+        this.nombre = nombre;
+        this.legajo = legajo;
+    }
+    getInfo() {
+        return `${this.nombre} (Leg. ${this.legajo})`;
+    }
+}
+class RegistroAlumnos {
+    constructor() {
+        this.alumnos = [];
+    }
+    agregar(alumno) {
+        this.alumnos.push(alumno);
+    }
+    listar() {
+        return this.alumnos;
+    }
+}
